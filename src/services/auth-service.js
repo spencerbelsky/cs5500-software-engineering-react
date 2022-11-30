15bd14615e986fff02dbcf9c49eefc6440c606ef
spencerbelsky <belsky.s@northeastern.edu>
2022-11-30 11:38:07 -0500

    a4

---
+++ src/services/auth-service.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+const BASE_URL = process.env.REACT_APP_BASE_URL
+//const BASE_URL = 'http://localhost:4000'
+//console.log(BASE_URL)
+
+const AUTH_API = `${BASE_URL}/api/auth`
+
+const api = axios.create({
+    withCredentials: true
+});
+
+export const signup = (user) =>
+    api.post(`${AUTH_API}/register`, user)
+        .then(response => response.data);
+
+export const login = (user) =>
+    api.post(`${AUTH_API}/login`, user)
+        .then(response => response.data);
+
+export const profile = () =>
+    api.post(`${AUTH_API}/profile`)
+        .then(response => response.data);
+
+export const logout = (user) =>
+    api.post(`${AUTH_API}/logout`, user)
+        .then(response => response.data);
--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -3,6 +3,11 @@ const BASE_URL = "http://localhost:4000";
 const TUITS_API = `${BASE_URL}/api/tuits`;
 const USERS_API = `${BASE_URL}/api/users`;
 
+const api = axios.create({
+    withCredentials: true
+});
+
+
 export const findAllTuits = () =>
   axios.get(TUITS_API)
     .then(response => response.data);
@@ -12,12 +17,12 @@ export const findTuitById = (tid) =>
     .then(response => response.data);
 
 export const findTuitByUser = (uid) =>
-  axios.get(`${USERS_API}/${uid}/tuits`)
-    .then(response => response.data);
+    api.get(`${USERS_API}/${uid}/tuits`)
+        .then(response => response.data);
 
-export const createTuitByUser = (uid, tuit) =>
-  axios.post(`${USERS_API}/${uid}/tuits`, tuit)
-    .then(response => response.data);
+export const createTuitByUser  = (uid, tuit) =>
+    api.post(`${USERS_API}/${uid}/tuits`, tuit)
+        .then(response => response.data);
 
 export const updateTuit = (tid, tuit) =>
   axios.post(`${TUITS_API}/${tid}`, tuit)
